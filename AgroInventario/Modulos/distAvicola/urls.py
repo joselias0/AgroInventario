c9cbddc8cea7a5c1ade2fdb2b7449a23ec6f2928
Elias <jose.morales3@catolica.edu.sv>
2024-04-07 15:17:44 -0600

    Realizacion pagina principal de agroinventario y pequeños cambios en el login

--- AgroInventario/Modulos/distAvicola/urls.py
+++ AgroInventario/Modulos/distAvicola/urls.py
@@ -7,5 +7,10 @@ from django.contrib.staticfiles.urls import static
 urlpatterns = [
     path('',index, name='index'),
     path('salir/', salir, name='salir' ),
+    path('huevos/',huevos, name='huevos'),
+    path('ventas/',ventas, name='ventas'),
+    path('recursos/',recursos, name='recursos'),
+    path('contabilidad/',contabilidad, name='contabilidad'),
+    path('cuidado/',cuidado, name='cuidado'),
 ]
 
